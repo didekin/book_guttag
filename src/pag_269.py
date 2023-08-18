c57ca27cae42ff97d08a3de199054776925e7203
pedroATdidekind <pedro@didekin.es>
2023-08-18 18:11:49 +0200

    Beginning plots

--- src/pag_269.py
+++ src/pag_269.py
@@ -3,7 +3,7 @@ def merge(left, right, compare):
     i, j = 0, 0
     while i < len(left) and j < len(right):
         if compare(left[i], right[j]):
-            result.append([left[i]])
+            result.append(left[i])
             i += 1
         else:
             result.append(right[j])
@@ -18,10 +18,19 @@ def merge(left, right, compare):
 
 
 def merge_sort(listin, compare=lambda x, y: x < y):
+    print(f'listinIn = {listin}')
     if len(listin) < 2:
         return listin[:]
     else:
         middle = len(listin) // 2
         left = merge_sort(listin[:middle], compare)
         right = merge_sort(listin[middle:], compare)
-        return merge(left, right, compare)
+        print(f'left = {left}, right = {right}')
+        merge_rec = merge(left, right, compare)
+        print(f'merge_rec = {merge_rec}')
+        return merge_rec
+
+
+intList = [(1, 2), (1, 0, 1), (2, 0, 0, 5), (1, 1, 1, 2, 3)]
+sort_intlist = merge_sort(intList, lambda x, y: sum(x) < sum(y))
+print(sort_intlist)
---
+++ src/trash.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [1, 7, 3, 5])
+plt.savefig('fig_1')
+# plt.show()
+
+
+# %%
+print('hola')
